4b81cefb1db480b0fc49c850792ef4bff8c22cbb
OdongAlican <sandieo.2020@gmail.com>
2020-04-24 11:19:59 +0300

    Adds condition to check if scores are invalid

--- bin/main.rb
+++ bin/main.rb
@@ -8,13 +8,24 @@ puts 'Player two'
 
 player_two = gets.chomp
 
+puts "Player one is #{player_one} and player two is #{player_two}"
+
+game_on = true
+
 puts ' | | '
 puts ' | | '
 puts ' | | '
+
 
 puts "#{player_one} what is your number?"
 
 score = gets.chomp.to_i
+
+# Declare an array to keep record of the scores
+scores_value = []
+
+# Push to the score to the array
+scores_value << score
 
 puts "#{player_one} your score is #{score}"
 
@@ -25,6 +36,21 @@ puts ' | | '
 puts "#{player_two} what is your number?"
 
 score = gets.chomp.to_i
+
+# Check if player two score is in the array
+if scores_value.include?(score)
+    puts "Invalid score! Please select another score"
+
+    # Request player two to enter another score
+    puts "#{player_two} what is your number?"
+
+    score = gets.chomp.to_i
+    
+    scores_value << score
+
+else
+    scores_value << score
+end
 
 puts "#{player_two} your score is #{score}"
 
@@ -63,6 +89,8 @@ puts ' |X|0'
 puts ' |X|0'
 
 puts "#{player_one} you win! #{score} got you there!"
+
+game_one = false
 
 puts 'Do you want to play again?'
 
