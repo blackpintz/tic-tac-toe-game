8fad1529622c4ea7eaf259d831b23dd67dbd3193
OdongAlican <sandieo.2020@gmail.com>
2020-04-29 11:11:29 +0300

    adds the board class

--- lib/game.rb
+++ lib/game.rb
@@ -22,3 +22,25 @@ end
 
 class Player_two < Player_one
 end
+
+class Board
+
+  attr_accessor :arry_one, :arry_two, :arry_three
+  def initialize(arry_one, arry_two, arry_three)
+    @arry_one = arry_one
+    @arry_two = arry_two
+    @arry_three = arry_three
+  end
+
+  def draw_board
+    t = table
+    t << @arry_one
+    t.add_separator
+    t << @arry_two
+    t.add_separator
+    t << @arry_three
+    t.add_separator
+    t
+  end
+
+end
