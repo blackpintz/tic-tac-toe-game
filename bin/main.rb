431248e133937595948115257a38e3d72cd804b5
Ubuntu <ubuntu@ip-172-31-35-111.us-east-2.compute.internal>
2020-05-01 12:46:11 +0000

    removes num_arr and winner global variables

--- bin/main.rb
+++ bin/main.rb
@@ -13,8 +13,10 @@ puts "player one is #{$player_one} and player two is #{$player_two}"
 game_arr = (1..9).to_a
 player = Player.new(0)
 token = player.token
+playing_board = Board.new(game_arr)
+board_arr = []
 
-def game_func(arr, game_arr,token)
+def game_func(arr, game_arr,token, playing_board)
   i = arr.length + 1
   while i.positive?
     if token == 0
@@ -22,65 +24,67 @@ def game_func(arr, game_arr,token)
       answer = gets.chomp
       if answer.to_i < 1 || answer.to_i > 9
         puts 'Number is out of range'
-        game_func(arr, game_arr,token)
+        game_func(arr, game_arr,token, playing_board)
       elsif arr.include?(answer.to_i)
         puts 'Number already selected'
-        game_func(arr, game_arr,token)
+        game_func(arr, game_arr,token, playing_board)
       else
         arr << answer.to_i
+        playing_board.change_board(answer, token)
+        puts playing_board.draw_board
+        puts playing_board.winner
         token = 1 
-        a_board = Board.new([1,2,3],[4,5,6])
       end
     else
       puts "#{$player_two}, what is your number?"
       answer = gets.chomp.to_i
       if answer.to_i < 1 || answer.to_i > 9
         puts 'Number is out of range'
-        game_func(arr, game_arr,token)
+        game_func(arr, game_arr,token, playing_board)
       elsif arr.include?(answer.to_i)
         puts 'Number already selected'
-        game_func(arr, game_arr,token)
+        game_func(arr, game_arr,token, playing_board)
       else
         arr << answer.to_i
+        playing_board.change_board(answer, token)
+        puts playing_board.draw_board
+        puts playing_board.winner
         token = 0
       end
     end
-    logic = GameLogic.new(game_arr)
-    puts token
-    puts logic.game(answer, token)
     i += 1
-    break if i == 10 || $winner == true
+    break if i == 10 || playing_board.winner
   end
   puts arr.length
-  if arr.length == 9 || $winner == true
-    def repeat(num_arr, game_arr, token)
+  if arr.length == 9 || playing_board.winner
+    def repeat(game_arr, token, playing_board)
       puts 'Do you wanna play again,. Press y if yes and n if no'
       ans = gets.chomp
       if ans == 'y' || ans == 'yes'
         arr = []
         token = 0
         game_arr = (1..9).to_a
+        playing_board= Board.new(game_arr)
         $board_number = []
-        $winner = false
-        game_func(arr, game_arr,token)
+        game_func(arr, game_arr,token, playing_board)
       elsif ans == 'n' || ans == 'no'
         puts 'Alright Guys. Get out of here'
         exit
       else
         puts 'Please Enter y or n'
-        repeat($num_arr, game_arr, token)
+        repeat(game_arr, token,playing_board)
       end
     end
-    if $winner
+    if playing_board.winner
       puts "#{$player_one}, you've won the game" if token == 1
       puts "#{$player_two}, you've won the game" if token == 0
-      repeat($num_arr, game_arr, token)
+      repeat(game_arr, token,playing_board)
     else
       puts 'Looks like its a draw game'
-      repeat($num_arr, game_arr, token)
+      repeat(game_arr, token, playing_board)
     end
-    repeat($num_arr, game_arr, token)
+    repeat(game_arr, token, playing_board)
   end
 end
 
-puts game_func($num_arr, game_arr, token)
+puts game_func(board_arr, game_arr, token, playing_board)
--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,9 @@
 require 'rubygems'
 require 'terminal-table/import'
 
-$num_arr = []
-$playing_numbers = (1..9).to_a
-$winner = false
+# $num_arr = []
+# $playing_numbers = (1..9).to_a
+# $winner = false
 
 class Player
   attr_accessor :token
@@ -13,68 +13,14 @@ class Player
 end
 
 class Board
-  attr_accessor :arry_one, :arry_two, :arry_three
-  def initialize(arry_one, arry_two, arry_three)
-    @arry_one = arry_one
-    @arry_two = arry_two
-    @arry_three = arry_three
-  end
-
-  def draw_board
-    board = table do |t|
-      t << @arry_one
-      t.add_separator
-      t << @arry_two
-      t.add_separator
-      t << @arry_three
-    end
-    board
-  end
-  
-  def vertical_check
-    check = nil
-    i = 0
-    while i < @arry_one.length
-      check = @arry_one[i] == @arry_two[i] && @arry_one[i] == @arry_three[i]
-      i += 1
-      break if check == true
-    end
-    check == true ? true : false
-  end
-  
-  def diagonal_check
-    i = 0
-    while i < @arry_one.length
-      check = (@arry_one[i] == @arry_two[i + 1] && @arry_one[i] == @arry_three[i + 2]) || (@arry_three[i] == @arry_two[i + 1] && @arry_three[i] == @arry_one[i + 2])
-      i += 1
-      break if check == true
-    end
-    check == true ?  true : false
-  end
-  
-  def linear_check
-    check = (@arry_one.all?(0) || @arry_one.all?('X')) || (@arry_two.all?(0) || @arry_two.all?('X')) || (@arry_three.all?(0) || @arry_three.all?('X'))
-    check == true ? true : false
-  end
-  
-  def winner
-    if linear_check || diagonal_check || vertical_check
-      true
-    else
-      false
-    end
-  end
-  
-end
-
-
-class GameLogic
+  attr_accessor :arr
+  @@board_number = nil
   def initialize(arr)
     @arr = arr
   end
 
-  def game(res, token)
-    val = res.to_i
+  def change_board(res, token)
+     val = res.to_i
     if @arr.include?(val)
       if token == 0
         @arr.map! do |value|
@@ -85,10 +31,55 @@ class GameLogic
           value == val ? value = 0 : value
         end
       end
-      $board_number = @arr.each_slice(3).to_a
-      my_board = Board.new($board_number[0], $board_number[1], $board_number[2])
-      puts my_board.winner
-      my_board.draw_board
+    @@board_number = @arr.each_slice(3).to_a
+    @@arry_one = @@board_number[0]
+    @@arry_two = @@board_number[1]
+    @@arry_three = @@board_number[2]
     end
+    
+    def draw_board 
+    board = table do |t|
+      t << @@arry_one
+      t.add_separator
+      t << @@arry_two
+      t.add_separator
+      t << @@arry_three
+    end
+    board
+    end
+  
+  def vertical_check
+    check = nil
+    i = 0
+    while i < @@arry_one.length
+      check = @@arry_one[i] == @@arry_two[i] && @@arry_one[i] == @@arry_three[i]
+      i += 1
+      break if check == true
+    end
+    check == true ? true : false
+  end
+  
+  def diagonal_check
+    i = 0
+    while i < @@arry_one.length
+      check = (@@arry_one[i] == @@arry_two[i + 1] && @@arry_one[i] == @@arry_three[i + 2]) || (@@arry_three[i] == @@arry_two[i + 1] && @@arry_three[i] == @@arry_one[i + 2])
+      i += 1
+      break if check == true
+    end
+    check == true ?  true : false
+  end
+  
+  def linear_check
+    check = (@@arry_one.all?(0) || @@arry_one.all?('X')) || (@@arry_two.all?(0) || @@arry_two.all?('X')) || (@@arry_three.all?(0) || @@arry_three.all?('X'))
+    check == true ? true : false
+  end
+  
+  def winner
+    if linear_check || diagonal_check || vertical_check
+      true
+    else
+      false
+    end
+  end
   end
 end
