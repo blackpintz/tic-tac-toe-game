19d8f919e47680fbebd79cbf1dda94692c4a3343
OdongAlican <sandieo.2020@gmail.com>
2020-05-06 20:03:12 +0300

    adds tests for vertical_check and diagonal_check methods

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -12,4 +12,33 @@ describe 'Game' do
       expect(board.change_board('p', 0)).not_to eql([7, 8, 9])
     end
   end
+  describe '#vertical_check' do
+    it 'checks a vertical win' do
+      @@arry_one = [1, 'x', 3]
+      @@arry_one = [1, 'x', 3]
+      @@arry_two = [4, 'x', 6]
+      @@arry_three = [7, 'x', 9]
+      expect(board.vertical_check).to be true
+    end
+    it 'checks when there is no vertical win' do
+      @@arry_one = [1, 'x', 3]
+      @@arry_two = [4, 'x', 6]
+      @@arry_three = [7, '0', 9]
+      expect(board.vertical_check).not_to be true
+    end
+  end
+  describe '#diagonal_check' do
+    it 'checks diagonal wins' do
+      @@arry_one = ['x', 2, 3]
+      @@arry_two = [4, 'x', 6]
+      @@arry_three = [7, 8, 'x']
+      expect(board.diagonal_check).to be true
+    end
+    it 'checks when there is no diagonal wins' do
+      @@arry_one = ['x', 2, 3]
+      @@arry_two = [4, 'x', 6]
+      @@arry_three = [7, 'x', 9]
+      expect(board.diagonal_check).not_to be true
+    end
+  end
 end
